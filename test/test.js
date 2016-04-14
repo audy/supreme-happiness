2a520be8f9333ee677eafebb0694032d0670e695
Austin Davis-Richardson <harekrishna@gmail.com>
2016-04-14 08:35:24 -0700

    initial commit

---
+++ test/test.js
@@ -0,0 +1,17 @@
+
+
+someFunction = function ( arg1, arg2  ) {
+        console.log(  ' this is a   test ') ;
+           }
+
+
+
+  console.log('wat');
+
+
+ // hello
+ //
+ //     world
+ //
+ //
+ //
