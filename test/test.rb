211c744ac1aa1ae0da59ebed02a178f6509d210f
Austin Davis-Richardson <harekrishna@gmail.com>
2016-04-14 08:38:40 -0700

    added ruby example

---
+++ test/test.rb
@@ -0,0 +1,21 @@
+
+class SomeClass     
+     def initialize
+  puts   "hello world!"
+     end
+
+
+
+     class << self
+       def foo
+         "bar"
+           end
+        end
+
+  end
+
+
+somehash = {
+    :key      => 'value', :otherkey => \
+         'value'
+}
